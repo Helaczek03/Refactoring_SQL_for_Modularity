044b20c4f0fb4986f6688ecc9801c8683ec0fc12
oskartajchert369@gmail.com <oskartajchert369@gmail.com>
2025-08-20 15:23:16 +0000

    Replacing source funtions with ref functions and renaming column names

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -4,48 +4,50 @@ with
 
 orders as (
     select *
-    from {{ source('jaffle_shop', 'orders') }}
+    from {{ ref('stg_jaffle_shop__orders') }}
 ),
 
 customers as (
     select *
-    from {{ source('jaffle_shop', 'customers') }}
+    from {{ ref('stg_jaffle_shop__customers') }}
 ),
 
 payments as (
     select *
-    from {{ source('stripe', 'payment') }}
+    from {{ ref('stg_stripe__payments') }}
 ),
 
 -- Logical CTEs
 completed_payments as (
     select 
-        orderid as order_id,
-        max(created) as payment_finalized_date,
-        sum(amount) / 100.0 as total_amount_paid
+        order_id,
+        max(payment_created_at) as payment_finalized_date,
+        sum(payment_amount) / 100.0 as total_amount_paid
     from
         payments
     where 
-        status <> 'fail'
+        payment_status <> 'fail'
     group by 1
 ),
 
 paid_orders as (
     select 
-        orders.id as order_id,
-        orders.user_id as customer_id,
-        orders.order_date as order_placed_at,
-        orders.status as order_status,
+        orders.order_id,
+        orders.customer_id,
+        orders.order_placed_at,
+        orders.order_status,
+
         completed_payments.total_amount_paid,
         completed_payments.payment_finalized_date,
-        customers.first_name as customer_first_name,
-        customers.last_name as customer_last_name
+
+        customers.customer_first_name,
+        customers.customer_last_name
     from
         orders
     left join completed_payments
-        on orders.id = completed_payments.order_id
+        on orders.order_id = completed_payments.order_id
     left join customers 
-        on orders.user_id = customers.id ),
+        on orders.customer_id = customers.customer_id ),
 
 final as (
     select
@@ -58,9 +60,11 @@ final as (
         customer_first_name,
         customer_last_name,
         
-        row_number() over (order by p.order_id) as transaction_seq,
+        --sales transaction sequence
+        row_number() over (order by order_id) as transaction_seq,
         
-        row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
+        --customer sales sequence
+        row_number() over (partition by customer_id order by order_id) as customer_sales_seq,
         
         --new vs returning customer
         case
@@ -75,17 +79,17 @@ final as (
 
         --customer lifetime value
         sum(total_amount_paid) over(
-            partition by paid_orders.customer_id
-            order by paid_orders.order_placed_at
-        ) as customer_lifetime_value
+            partition by customer_id
+            order by order_placed_at
+        ) as customer_lifetime_value,
 
         --first day of sale
-        first_value(paid_orders.order_placed_at) over(
-            partition by paid_orders.customer_id
-            order by paid_orders.order_placed_at
+        first_value(order_placed_at) over(
+            partition by customer_id
+            order by order_placed_at
         ) as fdos
     from 
-        paid_orders p
+        paid_orders
     order by 
         order_id)
 
