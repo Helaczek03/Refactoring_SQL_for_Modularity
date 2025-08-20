0c74f1bf66bb3d8af043bc3efb36d84c4950a525
oskartajchert369@gmail.com <oskartajchert369@gmail.com>
2025-08-20 15:27:01 +0000

    Changed total_amount_paid which had a mistake

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -22,7 +22,7 @@ completed_payments as (
     select 
         order_id,
         max(payment_created_at) as payment_finalized_date,
-        sum(payment_amount) / 100.0 as total_amount_paid
+        sum(payment_amount) as total_amount_paid
     from
         payments
     where 
