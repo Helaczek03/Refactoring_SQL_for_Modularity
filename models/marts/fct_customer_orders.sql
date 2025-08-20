c69c6bbbc6d5a129743b2c81112a883ff5d9d7d6
oskartajchert369@gmail.com <oskartajchert369@gmail.com>
2025-08-20 14:36:14 +0000

    Created import CTEs, Logical CTEs, Final CTE, changed some aliases into fullnames, changed CTE and joins into window functions

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,4 +1,35 @@
-with 
+--Import CTEs
+
+with
+
+orders as (
+    select *
+    from {{ source('jaffle_shop', 'orders') }}
+),
+
+customers as (
+    select *
+    from {{ source('jaffle_shop', 'customers') }}
+),
+
+payments as (
+    select *
+    from {{ source('stripe', 'payment') }}
+),
+
+-- Logical CTEs
+completed_payments as (
+    select 
+        orderid as order_id,
+        max(created) as payment_finalized_date,
+        sum(amount) / 100.0 as total_amount_paid
+    from
+        payments
+    where 
+        status <> 'fail'
+    group by 1
+),
+
 paid_orders as (
     select 
         orders.id as order_id,
@@ -10,61 +41,53 @@ paid_orders as (
         c.first_name    as customer_first_name,
         c.last_name as customer_last_name
     from
-        {{ source('jaffle_shop', 'orders') }}
-    left join (
-        select 
-            orderid as order_id,
-            max(created) as payment_finalized_date,
-            sum(amount) / 100.0 as total_amount_paid
-        from
-            {{ source('stripe', 'payment') }}
-        where 
-            status <> 'fail'
-        group by 1) p 
-    on orders.id = p.order_id
-    left join
-        {{ source('jaffle_shop', 'customers') }} c 
-    on orders.user_id = c.id ),
+        orders
+    left join completed_payments p 
+        on orders.id = p.order_id
+    left join customers c 
+        on orders.user_id = c.id ),
 
-customer_orders as (
-    select 
-        c.id as customer_id, 
-        min(order_date) as first_order_date, 
-        max(order_date) as most_recent_order_date, 
-        count(orders.id) as number_of_orders
+final as (
+    select
+        p.*,
+        row_number() over (order by p.order_id) as transaction_seq,
+        row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
+        
+        --new vs returning customer
+        case
+            when (
+                rank() over (
+                    partition by customer_id
+                    order by order_placed_at, order_id
+                ) = 1
+            ) then 'new'
+            else 'return' 
+        end as nvsr,
+        x.clv_bad as customer_lifetime_value,
+
+        --first day of sale
+        first_value(paid_orders.order_placed_at) over(
+            partition by paid_orders.customer_id
+            order by paid_orders.order_placed_at
+        ) as fdos
     from 
-        {{ source('jaffle_shop', 'customers') }} c 
-    left join 
-        {{ source('jaffle_shop', 'orders') }}
-    on orders.user_id = c.id 
-    group by 1)
+        paid_orders p
+        left outer join(
+            select
+                p.order_id,
+                sum(t2.total_amount_paid) as clv_bad
+            from 
+                paid_orders p
+            left join paid_orders t2 
+                on p.customer_id = t2.customer_id 
+                and p.order_id >= t2.order_id
+            group by 1
+            order by p.order_id
+        ) x 
+    on x.order_id = p.order_id
+    order by order_id)
 
-select
-    p.*,
-    row_number() over (order by p.order_id) as transaction_seq,
-    row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
-    case 
-        when c.first_order_date = p.order_placed_at
-        then 'new'
-        else 'return' 
-    end as nvsr,
-    x.clv_bad as customer_lifetime_value,
-    c.first_order_date as fdos
-from 
-    paid_orders p
-    left join customer_orders as c 
-    using (customer_id)
-    left outer join(
-        select
-            p.order_id,
-            sum(t2.total_amount_paid) as clv_bad
-        from 
-            paid_orders p
-        left join paid_orders t2 
-            on p.customer_id = t2.customer_id 
-            and p.order_id >= t2.order_id
-        group by 1
-        order by p.order_id
-    ) x 
-on x.order_id = p.order_id
-order by order_id
+select * from final
+
+-- created import CTEs, Logical CTEs, final CTE
+-- changed aliases to fullnames
